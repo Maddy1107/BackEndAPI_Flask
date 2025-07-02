7e1f0fad31ea1daf0cf21d29f9c24c2f18c02d58
Nilankar Deb <69686900+Maddy1107@users.noreply.github.com>
2025-07-02 23:03:01 +0530

    Update db_operations.py

--- Excel/db_operations.py
+++ Excel/db_operations.py
@@ -5,12 +5,18 @@ from database import db
 def insert_monthly_product_data(month, product_dict):
     db.session.query(MonthlyProductData).filter_by(month=month).delete()
 
-    for product_name, quantity, bottles in product_dict.items():
+    for product_name, values in product_dict.items():
+        if not isinstance(values, list) or len(values) < 2:
+            continue  # or raise an error
+
+        quantity = values[0]
+        bottles = values[1]
+
         entry = MonthlyProductData(
             month=month, product_name=product_name, quantity=quantity, bottles=bottles
         )
         db.session.add(entry)
-    db.session.commit()
+
     print(f"{len(product_dict)} products inserted for month: {month}")
 
 
