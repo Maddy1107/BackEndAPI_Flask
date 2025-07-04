98c79133ecbf3faf233bc1f37c01b6080123ad67
Nilankar Deb <69686900+Maddy1107@users.noreply.github.com>
2025-07-04 22:46:33 +0530

    Revert "Update database.py"
    This reverts commit 7ee788642726eb83043a36f82ee6c06c8972f93a.

--- Excel/database.py
+++ Excel/database.py
@@ -1,22 +1,15 @@
-import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from dotenv import load_dotenv
-
-load_dotenv()
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 
 def init_db(app):
-    uri = os.getenv("SUPABASE_DB_URI")
-
-    if not uri:
-        raise ValueError("Missing SUPABASE_DB_URI in environment variables")
+    uri = "postgresql://neondb_owner:npg_16dtTzOgnhfU@ep-broad-shape-a897iy1b-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require"
 
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
-    migrate.init_app(app, db)
+    migrate.init_app(app, db)  # ✅ Now you're using Flask-Migrate
